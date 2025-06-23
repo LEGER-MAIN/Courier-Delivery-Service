c86f7d43e8459897c426567936cb9ff83a0ca793
Jefferson Leger <legerbusiness2005@gmai.com>
2025-06-22 17:31:53 -0700

    Configurando JWT

--- Backend/utilities/sendMessage.js
+++ Backend/utilities/sendMessage.js
@@ -5,7 +5,7 @@ const client = require('twilio')(sid,token);
 
 const sendSMS = (to)=>{
     client.messages.create({
-        body:'Thank you for placing order with DelFe. Your order will be delivered soon!!!',
+        body:'Thank you for placing order with TeLlegaYa. Your order will be delivered soon!!!',
         to:`+91${to}`,
         from:'+12344234645'
     }).then(res=>console.log(res))
